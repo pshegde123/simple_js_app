e558749befd83346b5745f0ca4426755f58d5e5a
phegde <pradnyahegde.null@gmail.com>
2024-11-26 10:49:48 -0500

    Added four objects named 'Charmeleon','Bulbasaur',''Pikachu','Wartortle' to pokemonList array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-alert('Hello world');
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name:'Charmeleon',
+        height:11,
+        types:['Dragon','Monster','Mountain']
+    },
+    {
+        name: 'Bulbasaur', 
+        height: 7, 
+        types: ['Grass', 'Monster']
+    },{
+        name:'Pikachu',
+        height:4,
+        types:['Fairy','Field']        
+    },{
+        name:'Wartortle',
+        height:10,
+        types:['Monster','Water 1']        
+    }
+];
