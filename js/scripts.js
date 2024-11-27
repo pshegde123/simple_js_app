bdb83ad7d20a97dcc2193855eed616a2d52a55d5
phegde <pradnyahegde.null@gmail.com>
2024-11-27 11:53:21 -0500

    Added code to print pokemon name and height on the page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
 let pokemonList = [
-    {
-        name:'Charmeleon',
-        height:11,
-        types:['Dragon','Monster','Mountain']
-    },
-    {
-        name: 'Bulbasaur', 
-        height: 7, 
-        types: ['Grass', 'Monster']
-    },{
-        name:'Pikachu',
-        height:4,
-        types:['Fairy','Field']        
-    },{
-        name:'Wartortle',
-        height:10,
-        types:['Monster','Water 1']        
+  {
+    name: 'Charmeleon',
+    height: 1.1,
+    types: ['Dragon', 'Monster', 'Mountain']
+  },
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['Grass', 'Monster']
+  },
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    types: ['Fairy', 'Field']
+  },
+  {
+    name: 'Wartortle',
+    height: 1,
+    types: ['Monster', 'Water 1']
+  }
+]
+
+for(let i=0;i<pokemonList.length;i++){
+    //document.write(pokemonList[i].name `(${pokemonList[i].height})`);
+    if(pokemonList[i].height > 1){
+        document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big! </p>`);    
+    }else{
+        document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height})</p>`);    
     }
-];
+    
+}
