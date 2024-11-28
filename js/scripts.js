df70f9b0e7774e6cc4ba22e1c410a489b7f70eed
phegde <pradnyahegde.null@gmail.com>
2024-11-28 08:56:56 -0500

    Added function getAll(), add() and filterByName()

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,72 @@
-let pokemonList = [
-  {
-    name: 'Charmeleon',
-    height: 1.1,
-    types: ['Dragon', 'Monster', 'Mountain']
-  },
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ['Grass', 'Monster']
-  },
-  {
-    name: 'Pikachu',
-    height: 0.4,
-    types: ['Fairy', 'Field']
-  },
-  {
-    name: 'Wartortle',
-    height: 1,
-    types: ['Monster', 'Water 1']
-  }
-]
+var pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Charmeleon",
+      height: 1.1,
+      types: ["Dragon", "Monster", "Mountain"],
+    },
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["Grass", "Monster"],
+    },
+    {
+      name: "Pikachu",
+      height: 0.4,
+      types: ["Fairy", "Field"],
+    },
+    {
+      name: "Wartortle",
+      height: 1,
+      types: ["Monster", "Water 1"],
+    },
+  ];
 
-pokemonList.forEach(function (pokemon) {
+  function getAll() {
+    return pokemonList;
+  }
+
+  function filterByName(searchPokemonName) {
+    var result = pokemonList.filter((pokemon) => {
+      return pokemon.name === "Pikachu";
+    });
+    //console.log("result = ", result);
+    return result;
+  }
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("Incorrect type of pokemon.");
+    }
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+    filterByName: filterByName,
+  };
+})();
+
+/* Show name & height of each pokemon */
+pokemonRepository.getAll().forEach(function (pokemon) {
   if (pokemon.height > 1) {
     document.write(
       `<p>${pokemon.name} (height: ${pokemon.height}) - Wow, that's big! </p>`
-    )
+    );
   } else {
-    document.write(`<p>${pokemon.name} (height: ${pokemon.height})</p>`)
+    document.write(`<p>${pokemon.name} (height: ${pokemon.height})</p>`);
   }
-})
+});
+
+/* Filter by name */
+var searchResult = pokemonRepository.filterByName("Pikachu");
+document.write(
+  `<p>Filter By Name:</p><p>${searchResult[0].name} ,height: ${searchResult[0].height}, types=${searchResult[0].types}</p>`
+);
